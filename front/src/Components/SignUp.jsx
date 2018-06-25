0d72cc4af1b52b814238a9b12e3b4f6f16cba0b6
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-06-25 15:39:26 +0200

    syncronisation de la balise h1 et du formulaire

--- front/src/Components/SignUp.jsx
+++ front/src/Components/SignUp.jsx
@@ -5,20 +5,70 @@ export default class SignUp extends Component {
         super(props);
         this.state = {
             email: "mail@default.com",
+            password: "",
+            lastname: "",
+            firstname: "",
         };
-        this.updateEmailField = this.updateEmailField.bind(this);
+        this.updateMail = this.updateMail.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        this.updateLastname = this.updateLastname.bind(this);
+        this.updateFirstname = this.updateFirstname.bind(this);
+
     }
 
-    updateEmailField(e) {
+    updateMail(e) {
         this.setState({
-            email: e.target.value
+            email: e.target.value,
+        });
+    }
+
+    updatePassword(e) {
+        this.setState({
+            password: e.target.value,
+        });
+    }
+
+    updateLastname(e) {
+        this.setState({
+            lastname: e.target.value,
+        });
+    }
+
+    updateFirstname(e) {
+        this.setState({
+            firstname: e.target.value,
         });
     }
 
     render () {
-        return (<div>
-            <h1>{this.state.email}</h1>
-            <input type="email" name="email" onChange={this.updateEmailField} />
-        </div>);
+        return (
+        <div>
+            <h1>{JSON.stringify(this.state)}</h1>
+            <form>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="email" value={this.state.email} onChange={this.updateMail} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" value={this.state.password} name="password" onChange={this.updatePassword} />
+                </div>
+                <div>
+                    <label>Password confirmation</label>
+                    <input type="password" name="password_verif" />
+                </div>
+                <div>
+                    <label>Nom</label>
+                    <input type="text" value={this.state.lastname} name="lastname" onChange={this.updateLastname} />
+                </div>
+                <div>
+                    <label>Prénom</label>
+                    <input type="text" value={this.state.firstname} name="firstname" onChange={this.updateFirstname} />
+                </div>
+                
+                <input type="submit" value="Soumettre" />
+            </form>
+        </div>
+        );
     }
 }
